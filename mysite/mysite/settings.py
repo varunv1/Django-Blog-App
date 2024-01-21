4650b586c54b4f61b2129a2afd9a889e609dfd27
Varun <varunv1a2r3@gmail.com>
2024-01-21 22:54:44 +0530

    added view, url and templates

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "blog.apps.BlogConfig"
 ]
 
 MIDDLEWARE = [
@@ -99,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "ASIA/KOLKATA"
 
 USE_I18N = True
 
