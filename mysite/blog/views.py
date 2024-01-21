9b8cd5d7e67c4595c81a71b56228071a08f5825e
Varun <varunv1a2r3@gmail.com>
2024-01-21 23:08:06 +0530

    finished chapter

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,7 +5,6 @@ from django.http import Http404
 
 def post_list(request):
     posts = Post.published.all()
-    print(posts, 'posts')
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, id):
